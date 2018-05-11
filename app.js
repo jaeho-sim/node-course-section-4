bb649aa2bba60fe8c87d0b52bf01d58a1227da21
Daria Sirous <dsirous@tribalscale.com>
2018-05-11 12:39:55 -0400

    axios

--- app.js
+++ app.js
@@ -4,18 +4,6 @@ const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
 
-const argv = yargs.options({
-  a: {
-    demand: true,
-    alias: 'address',
-    describe: 'Address to fetch weather for',
-    string: true
-  }
-})
-.help()
-.alias('help', 'h')
-.argv;
-
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if(errorMessage) {
     console.log(errorMessage);
@@ -32,4 +20,3 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     });
   }
 });
-
--- playground/promise.js
+++ playground/promise.js
@@ -1,3 +1,33 @@
-var somePromise = new Promise(() => {
-  
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      }
+      else {
+        reject('Arguments must be numbers');
+      }
+    }, 1500);
+  });
 };
+
+asyncAdd(5,6).then((res) => {
+  console.log('result: ', res);
+  return asyncAdd(res, 33);
+}).then((res) => {
+  console.log('shuld be 43', res);
+}.catch((errorMessage) => {
+  console.log(errorMessage);
+});
+
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('Hey it worked');
+//   }, 2500);
+// });
+//
+// somePromise.then((message) => {
+//   console.log('Success: ', message);
+// }, (errorMessage) => {
+//   console.log('Error: ', errorMessage);
+// });
